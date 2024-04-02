2932584176d5a1769109bf3a1ded909806f4a628
vikusher <vikusher@gmail.com>
2024-04-01 21:14:36 -0400

    Fixed congig reader class #4

--- src/com/techcenture/academy/config/ConfigReader.java
+++ src/com/techcenture/academy/config/ConfigReader.java
@@ -1,8 +1,9 @@
 package com.techcenture.academy.config;
 
 public class ConfigReader {
-    public static void main(String[] args) {
 
-        System.out.println("hello there");
+
+    public static String getBroweser() {
+        return "dgf yvd";
     }
 }
