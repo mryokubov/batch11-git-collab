120e396e7d5e353bd9d81de16d93f8f66b33ddd9
TairovichJR <yokubov.omonjon@gmail.com>
2024-04-01 21:06:45 -0400

    implementing excel reader class closes #3

--- src/com/techcenture/academy/utils/ExcelReader.java
+++ src/com/techcenture/academy/utils/ExcelReader.java
@@ -1,4 +1,11 @@
 package com.techcenture.academy.utils;
 
 public class ExcelReader {
+
+    public ExcelReader() {
+    }
+
+    public Object[][] getData(){
+        return new Object[][]{};
+    }
 }
